34a509d8a077140f43da52631b10f92e7a9e7030
loenardo12 <cakkranegara@gmail.com>
2025-02-25 09:22:24 +0700

    first second

---
+++ app/Http/Controllers/newsapitube.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+class newsapitube extends Controller
+{
+    public function makeRequest(Request $request)
+	{
+		$url = "https://api.apitube.io/v1/news/everything?per_page=50&api_key=api_live_ew3bRhaMn6N9SEqva3ZAxZISQw0WlAZvYOPQxlJhTxQA3SWyxI9fXG6oG3Yz";
+
+		$response = Http::get($url);
+
+		if ($response->failed()) {
+			$errorMessage = $response->clientError()
+				? "Client Error: " . $response->status()
+				: "Server Error: " . $response->status();
+
+			echo "HTTP Request Error: " . $errorMessage;
+		} else {
+			echo $response->body();
+		}
+	}
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,15 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\newscontroller;
+// Route::get('/', function () {
+//     // return view('welcome');
+//     return redirect('/news');
+// });
+
+// Route::get('/news',[newsController::class,'index'])->name('index');
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
+
