f946abc5fed3f6940c6a4bcc810df2e1cc0b867e
loenardo12 <cakkranegara@gmail.com>
2025-02-25 10:49:03 +0700

    tree

--- routes/web.php
+++ routes/web.php
@@ -2,14 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\newscontroller;
-// Route::get('/', function () {
-//     // return view('welcome');
-//     return redirect('/news');
-// });
+Route::get('/', function () {
+    return view('welcome');
+    // return redirect('/news');
+});
 
 // Route::get('/news',[newsController::class,'index'])->name('index');
 
-Route::get('/', function () {
-    return view('index');
-});
 
